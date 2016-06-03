3b3de467ed65f1a9fa717750bdc047b9e2d80461
Douglas May <Dam1403@rit.edu>
2016-06-03 14:33:56 -0400

    RamsController Comments
    The RamsController now contains more detailed information about the
    inner workings of the controller

--- laravel/app/controllers/RamsController.php
+++ laravel/app/controllers/RamsController.php
@@ -2,17 +2,24 @@
 
 class RamsController extends BaseController {
 
-
+  /*
+    create - Creates a new ram object and inserts it into the database
+  */
 	public function create()
 	{
+    #Get the statistic object
     $stats = statistic::find(1);
+
     if(is_null($stats)){
       return 'Statistical Error';
     }
+    #Get the Ram object
 		$ramEntry = new Ram;
     if (Input::has('ram_amount'))
     {
+      #set ramEntries amountBytes
       $ramEntry->amountBytes = Input::get('ram_amount');
+      #add the current amountBytes to the total ram section of the statistic row
       $stats->total_ram = $stats->total_ram + $ramEntry->amountBytes;
     }
     else{
@@ -29,7 +36,12 @@ class RamsController extends BaseController {
     $stats->save();
     return Redirect::to('/')->with('message','Behold... Your computer now harnesses the power of 1000 suns');
 	}
+  /*
+    getinfo - This returns the statistics displayed on the info.blade.php
+    view. This function handles the '/' get request.
+  */
   public function getinfo(){
+    #get statistic object
     $stats = statistic::find(1);
     if(is_null($stats)){
       return 'Statistical Error';
@@ -38,7 +50,10 @@ class RamsController extends BaseController {
 
     return View::make('info')->with('stats',$stats);
   }
-
+  /*
+    This function processes the complaint post request from the complain.blade.php
+    view
+  */
   public function complaints(){
     $stats = statistic::find(1);
     if(is_null($stats)){
